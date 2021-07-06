9a2e45c4008a342148838805e15a2b9677359f13
samara oliveira <samaraoliveira018@gmail.com>
2021-07-06 00:31:12 -0400

    criando rotas home,ranking e iniciar jogo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-          <h1> Monster Hunter</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+            <h1>Home</h1>
+        </Route>
+        <Route  exact path="/ranking">
+            <h1>Ranking</h1>
+        </Route>
+        <Route exact path="/iniciar-jogo">
+            <h1>Iniciar Jogo</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
