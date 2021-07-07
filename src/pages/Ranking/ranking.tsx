668c6329fa1b1f67aecd331b61c19c96c6cffbce
samara oliveira <samaraoliveira018@gmail.com>
2021-07-07 14:42:21 -0400

    ajustando barra de navegação da página ranking

---
+++ src/pages/Ranking/ranking.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../../components/navbar';
+
+// import { Container } from './styles';
+
+const Ranking: React.FC = () => {
+    return (
+        <Navbar></Navbar>
+        );
+}
+
+export default Ranking;
