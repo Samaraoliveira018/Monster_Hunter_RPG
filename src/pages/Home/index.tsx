e3ff63e9f6ef34b203ab3d2dfb437d33ffaf411a
samara oliveira <samaraoliveira018@gmail.com>
2021-07-07 14:38:02 -0400

    ajustando layout da página home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
-// import { Container } from './styles';
+import { Button } from 'react-bootstrap';
+import './styles.css';
 
 const Home: React.FC = () => {
 
   return (
-    <Navbar>
-      <div> 
-        
+    <>
+      <Navbar></Navbar>
+      <div className="card">
+        <div className="area">
+          <form className="formu">
+
+            <div className='name'>
+
+            <label>Nome do Jogador</label>
+            <input type='text'/>
+            </div>
+            <Button type='submit'>iniciar jogo</Button>
+          </form>
+        </div>
       </div>
-    </Navbar>
+    </>
   );
 }
 
