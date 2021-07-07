c562d3385e2c77a417cbd2d17bade6eb3d6d802a
samara oliveira <samaraoliveira018@gmail.com>
2021-07-07 14:39:33 -0400

    finalizando navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,32 +1,32 @@
 
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
- import  './style.css';
+import './style.css';
 
-const Navbar: React.FC = ({children}) => {
+const Navbar: React.FC = ({ children }) => {
     return (
         <div>
-        <nav className='header'>
-            <div className="menu"></div>
-            <div className="line1"></div>
-            <div className="line2"></div>
-            <div className="line3"></div>
-            <ul className='nav-list'>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">Game</Link>
-                </li>
-                <li>
-                    <Link to="/users">Users</Link>
-                </li>
-            </ul>
-        </nav>
-        {children}
+            <nav className='header'>
+                <div className="menu"></div>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+                <ul className='navlist'>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/game">Game</Link>
+                    </li>
+                    <li>
+                        <Link to="/ranking">Ranking</Link>
+                    </li>
+                </ul>
+            </nav>
+            {children}
         </div>
-        );
+    );
 }
 
 export default Navbar;
